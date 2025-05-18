7fd62cf53f0c14b40cb04572b9f1dc1b5cc57992
Her01na <teodorsvoitkevics@gmail.com>
2025-05-18 15:18:26 +0100

    Update open_table_db.sql

--- open_table_db.sql
+++ open_table_db.sql
@@ -1,1 +1,7 @@
+--------------------------------
+#Template to edit and modify sql
+--------------------------------
 SELECT * FROM student_registrations;
+SELECT * FROM campuses;
+SELECT * FROM events;
+SELECT * FROM student_event_registrations;
